dcf5bbd57d967badf1e8bef1c7bd6b1aea929355
Almas Almas <108171716+minu312@users.noreply.github.com>
2026-04-04 12:59:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,12 +59,15 @@ def query_text(inline_query):
 
 # ================= FLASK WEB PANEL (BOOTSTRAP) =================
 
-@app.route(f'/{BOT_TOKEN}', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return '!', 200
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return '!', 200
+    else:
+        return 'Error', 403
 
 @app.route('/')
 def admin_panel():
